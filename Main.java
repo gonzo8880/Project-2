8b741dbe80d9236be94f49678c6195db35a77fbc
gonzo8880 <gonzaloscity@gmail.com>
2020-07-27 01:39:06 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+Scanner;class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
+public class MyClass {
+  public static void main(String[] args) {
+    String[] cars = {"Compact", "Toyota", "Chevrolet", "Kia"}
+    for (String i cars) {
+      System.out.println(i);
+    }
+  }  
+}
+
+}
+
+import java.util.Scanner; Computer Science
+
+class MyClass {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in); 
+    System.out.println("Gonzalo");
+
+    String username = myObj.nextline();
+    System.out.println("Username is: " + userName);
+  }
+}
+public class MyClass {
+  public static void main(String[] args) {
+    int x = 30;
+    int y = 15;
+    if (x > y) {
+      System.out.println ("x is greater than y");
+    }
+  }
+}
